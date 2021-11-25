6602b72a6626dcc7c3a74f8ed64d91d2bf47c8f3
volkanyenibagci <volkanyenibagci@gmail.com>
2021-11-25 15:20:29 +0300

    Register and Login

---
+++ AspNetWebApp/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetWebApp/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using AspNetWebApp.Models.Siniflar;
+
+namespace AspNetWebApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        
+        Context c = new Context();
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult KullaniciEkle(Admin a)
+        {
+
+            //TODO Durum True yapılacak listeye gelmesi için
+            
+            c.Admins.Add(a);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
